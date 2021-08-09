4f74696a7adaf9c95ce575d9dd89c4138e3bb616
Hernán Amarillo <hernan.amarillo@kaplan.edu>
2021-08-09 01:06:07 -0300

    Fix of UT

--- src/app/shared/components/error-message/error-message.component.spec.ts
+++ src/app/shared/components/error-message/error-message.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBarModule, MatSnackBarRef, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 
 import { ErrorMessageComponent } from './error-message.component';
 
@@ -6,9 +7,25 @@ describe('ErrorMessageComponent', () => {
   let component: ErrorMessageComponent;
   let fixture: ComponentFixture<ErrorMessageComponent>;
 
+  const MockSnackBarData = {
+    info: {
+      explanation: 'an explanation',
+      message: 'an error',
+      recommendation: 'a recommendation'
+    },
+    closeText: 'OK'
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ErrorMessageComponent ]
+      declarations: [ ErrorMessageComponent ],
+      imports: [
+        MatSnackBarModule
+      ],
+      providers: [
+        { provide: MatSnackBarRef, useValue: null },
+        { provide: MAT_SNACK_BAR_DATA, useValue: MockSnackBarData }
+      ]
     })
     .compileComponents();
   });
--- src/app/shared/services/cities.service.spec.ts
+++ src/app/shared/services/cities.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from  '@angular/common/http/testing';
 
 import { CitiesService } from './cities.service';
 
@@ -6,7 +7,9 @@ describe('CitiesService', () => {
   let service: CitiesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(CitiesService);
   });
 
